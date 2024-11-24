f2bdca2c02f8c92aaab74d3aca2e1a698bec92c0
Darko Radivojevic <darko_king2@hotmail.com>
2024-11-24 21:52:17 +0100

    Add integration tests

---
+++ Handler/Tests/Integration/MongoTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+
+namespace Handler.Tests.Integration
+{
+    [TestClass]
+    public class MongoTests
+    {
+        public const string ConnectionString = "mongodb://localhost:27017";
+        public const string Database = "Test";
+        public const string Collection = "test-collection";
+
+        private static MongoClient _sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sut = new MongoClient(ConnectionString);
+        }
+
+        [TestMethod]
+        public async Task Mongo_CanPull()
+        {
+            var database = _sut.GetDatabase(Database);
+            var collection = database.GetCollection<object>(Collection);
+
+            var items = await collection.Find(_ => true).ToListAsync();
+
+            Assert.IsNotNull(collection);
+            Assert.IsFalse(items.Any());
+        }
+
+        [TestMethod]
+        public async Task Mongo_CanInsert()
+        {
+            var database = _sut.GetDatabase(Database);
+            var collection = database.GetCollection<object>(Collection);
+
+            await collection.InsertOneAsync(new object());
+        }
+
+        [TestCleanup]
+        public async Task TestCleanup()
+        {
+            var database = _sut.GetDatabase(Database);
+            await database.DropCollectionAsync(Collection);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _sut.DropDatabase(Database);
+        }
+    }
+}
